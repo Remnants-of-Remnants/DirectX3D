830b7794c6ca6a6313cd25ec0ce0cc8a7280395d
oljindudol <82567430+oljindudol@users.noreply.github.com>
2024-06-07 08:45:44 +0900

    #151 state region 설정

--- Project/Scripts/CPlayerScript.h
+++ Project/Scripts/CPlayerScript.h
@@ -28,17 +28,19 @@ public:
     virtual void Overlap(CCollider2D* _Collider, CGameObject* _OtherObj, CCollider2D* _OtherCollider);
     virtual void EndOverlap(CCollider2D* _Collider, CGameObject* _OtherObj, CCollider2D* _OtherCollider);
 
-
     virtual void SaveToFile(FILE* _File) override;
     virtual void LoadFromFile(FILE* _File) override;
     CLONE(CPlayerScript);
 
+#pragma region About State
     void NormalBegin();
     int NormalUpdate();
     void NormalEnd();
     void AttackBegin();
     int AttackUpdate();
     void AttackEnd();
+#pragma endregion
+
 public:
     CPlayerScript();
     ~CPlayerScript();
